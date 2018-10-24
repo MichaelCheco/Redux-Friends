819e737546ba6c422d2c3dd31b8f308a89f21b8b
Michael Checo <checomichael2@gmail.com>
2018-10-24 19:40:42 -0400

    initialized state and reducer

---
+++ friends/src/reducers/friendsReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  friends: [],
+  fetchingFriends: false,
+  friendsFetched: false,
+  error: null
+};
+
+export const friendsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+        return state
+    }
+}
+
