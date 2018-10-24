aada11f7ecc673b5b80a0d9034a50b02bb79a3f7
Michael Checo <checomichael2@gmail.com>
2018-10-24 19:30:29 -0400

    initial commit

---
+++ friends/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore, applyMiddleware} from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+const store = createStore(() => {}, applyMiddleware(logger, thunk))
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>,
+document.getElementById('root'));
+
+
